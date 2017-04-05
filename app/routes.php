9e7bdb804c66f19ef554ca92a1e28c8a0b1ee221
Mohamad Rashid <mohamadrashid@Mohamads-iMac.lan>
2017-04-05 22:17:12 +0200

    fixed untracked files

--- app/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$app->get('/','HomeController:index');
-$app->get('/auth/signup','AuthController:getSignUp')->setName('auth.signup');
-$app->post('/auth/signup','AuthController:postSignUp');
