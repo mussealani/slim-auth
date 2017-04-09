a4f5f38178436fa711103c1353e0262130fa77ba
Mohamad Rashid <mohamadrashid@Mohamads-iMac.lan>
2017-04-09 16:07:05 +0200

    Created postSignUp method ”AuthController”.

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
+use App\Models\User;
 
 
 class AuthController extends Controller
@@ -20,8 +21,13 @@ class AuthController extends Controller
   /**
    *
    */
-  public function postSignUp()
+  public function postSignUp($req, $res)
   {
-
+   $user =  User::create([
+      'email' => $req->getParam('email'),
+      'name' => $req->getParam('name'),
+      'password' => password_hash( $req->getParam('password'), PASSWORD_DEFAULT ),
+    ]);
+   return $res->withRedirect($this->router->pathFor('home'));
   }
 }
