7d5041701dd6a53ef5c08dcbf35aa7c50c2fb68c
Mohamad Rashid <mohamadrashid@Mohamads-iMac.lan>
2017-04-13 20:52:44 +0200

    Created PasswordController

---
+++ app/Controllers/Auth/PasswordController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\Controller;
+use App\Models\User;
+use Respect\Validation\Validator as v;
+
+
+class PasswordController extends Controller
+{
+  /**
+   * @param $req
+   * @param $res
+   */
+  public function getChangePassword($req, $res)
+  {
+    $this->view->render($res, 'auth/password/change.twig');
+  }
+
+  /**
+   * @param $req
+   * @param $res
+   * @return mixed
+   */
+  public function postChangePassword($req, $res)
+  {
+    $validation = $this->validator->validate($req, [
+      'password_old' => v::noWhitespace()->notEmpty()->matchesPassword($this->auth->user()->password),
+      'password' => v::noWhitespace()->notEmpty(),
+    ]);
+
+    if ($validation->failed()) {
+
+      return $res->withRedirect($this->router->pathFor('auth.password.change'));
+    }
+  }
+}
